8c6541a1eda04bf06303d534ca3e190886ac30eb
sawyersieja <sawyersieja@protonmail.com>
2024-08-19 13:44:37 -0700

    Clicking outside the menu area with menu open now closes menu.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,32 @@
-document.getElementById('menu-toggle').addEventListener('change', function() {
-    document.getElementById('hamburger-line-top').classList.toggle('rotate-45');
-    document.getElementById('hamburger-line-mid').classList.toggle('hide');
-    document.getElementById('hamburger-line-bot').classList.toggle('rotate--45');
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+    const menuContainer = document.getElementById('menu-container');
+    const hamburgerLines = {
+        top: document.getElementById('hamburger-line-top'),
+        mid: document.getElementById('hamburger-line-mid'),
+        bot: document.getElementById('hamburger-line-bot')
+    };
+
+
+    document.getElementById('menu-toggle').addEventListener('change', function() {
+        if (this.checked) {
+            menu.classList.add('scale-y-100');
+            hamburgerLines.top.classList.add('rotate-45');
+            hamburgerLines.mid.classList.add('hide');
+            hamburgerLines.bot.classList.add('rotate--45');
+        } else {
+            menu.classList.remove('scale-y-100');
+            hamburgerLines.top.classList.remove('rotate-45');
+            hamburgerLines.mid.classList.remove('hide');
+            hamburgerLines.bot.classList.remove('rotate--45');
+        }   
+    });
+
+    //Closes menu when clicking outside the menu
+    document.addEventListener('click', function(event) {
+        if (!menuContainer.contains(event.target) && menuToggle.checked) {
+            menuToggle.checked = false;
+        }
+    });
 });
